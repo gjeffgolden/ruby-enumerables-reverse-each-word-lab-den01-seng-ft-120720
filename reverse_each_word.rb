454263cc31ca5228739773095e05d2989f80b458
Jeff Golden <gjgolden08@gmail.com>
2020-11-26 19:43:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(string)
   array=[]
   array << string.split(" ")
-  
+  reversed_array = []
+  array.each do |index|
+   reversed_array << index.reverse!
   end
+    return reversed_array
 end
     
